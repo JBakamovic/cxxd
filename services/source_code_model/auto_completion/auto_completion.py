5b4a505216f68705920258777c5d847e215b805d
Jusufadis Bakamovic <jbakamovic@localhost.localdomain>
2018-05-08 16:47:23 +0200

    Handle invalid expressions case better. Remove logs.

--- services/source_code_model/auto_completion/auto_completion.py
+++ services/source_code_model/auto_completion/auto_completion.py
@@ -25,22 +25,15 @@ class AutoCompletion():
 
         completion_candidates = []
         s = linecache.getline(contents_filename, line)
-        logging.info('line = {0}'.format(s))
         idx = extract_symbol(s[0:column])
-        logging.info('idx = {0}'.format(column - idx))
         if idx != -1:
             expression = s[(column-idx):column]
+            for result in self.auto_complete.results:
+                for completion_chunk in result.string:
+                    if completion_chunk.isKindTypedText() and expression in completion_chunk.spelling:
+                        completion_candidates.append(result)
+                        break
         else:
-            expression = ' hahahhahahah '
-        logging.info('Expression = {0}, Line = {1}, column = {2}'.format(expression, line, column))
-        for result in self.auto_complete.results:
-            for completion_chunk in result.string:
-                if completion_chunk.isKindTypedText() and expression in completion_chunk.spelling:
-                    completion_candidates.append(result)
-                else:
-                    pass
-
-        for candidate in completion_candidates:
-            logging.info('{0}'.format(candidate))
+            pass # Can't do nothing about it ...
 
         return self.auto_complete is not None, completion_candidates
