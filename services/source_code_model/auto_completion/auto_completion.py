d6601b250eb2b46bde689ee68490f9924c6bf854
Jusufadis Bakamovic <jbakamovic@localhost.localdomain>
2018-05-10 14:00:16 +0200

    WIP: handle refine-candidates implicitly
    This will move away the burden from client implementation.

--- services/source_code_model/auto_completion/auto_completion.py
+++ services/source_code_model/auto_completion/auto_completion.py
@@ -40,30 +40,94 @@ class AutoCompletion():
         original_filename = str(args[0])
         contents_filename = str(args[1])
         line              = int(args[2])
+        column            = int(args[3]) - 1
+        offset            = int(args[4])
+
+        line_string = read_line(contents_filename, offset)
+        logging.info("Filtering from [{0}, {1}] with offset = {2}: line = '{3}', len = {4}".format(line, column, offset, line_string, len(line_string)))
+
+        last_char_idx = column - 1
+        if last_char_idx < len(line_string):
+            last_char_input = line_string[last_char_idx-1]
+            logging.info("last_char_input = '{0}'".format(last_char_input))
+            if last_char_input == '\n' or last_char_input == '\r':
+                logging.info('Carriage return detected ...')
+                pass
+            elif last_char_input.isspace() or last_char_input == ',' or last_char_input == ';' or last_char_input == '=':
+                logging.info('Delimiting character detected ... Clearing up the completion candidates.')
+                del self.completion_candidates[:]
+            else:
+                logging.info("Will extract symbol from '{0}'".format(line_string[0:last_char_idx]))
+                logging.info('Trying harder ...')
+                idx = extract_symbol(line_string[0:last_char_idx])
+                if idx != -1:
+                    expression = line_string[(last_char_idx-idx):last_char_idx] #.rstrip()
+                    logging.info("expression found = '{0}'".format(expression))
+                    last_char_input = expression[len(expression)-1] if len(expression) else ''
+                    logging.info("last_char_input = '{0}'".format(last_char_input))
+                    if last_char_input == ':' or last_char_input == '>' or last_char_input == '.' or last_char_input == '' or len(self.completion_candidates) == 0 and last_char_input != '-':
+                        logging.info('Getting new list of candidates ...')
+                        del self.completion_candidates[:]
+                        tunit = self.parser.parse(contents_filename, original_filename)
+                        self.auto_complete = self.parser.auto_complete(
+                                tunit, line, column + 1
+                        )
+                        self.completion_candidates = self.__filter_completion_candidates(self.auto_complete.results, expression) # At this point we might already have something to work with (e.g. part of the string we may trigger filtering with)
+                    else:
+                        logging.info("Filtering by expression = '{0}' from '{1}' starting from {2}".format(expression,
+                            line_string[0:last_char_idx], idx))
+                        self.completion_candidates = self.__filter_completion_candidates(self.completion_candidates, expression) # At this point we might already have something to work with (e.g. part of the string we may trigger filtering with)   
+                    logging.info('Found {0} candidates.'.format(len(self.completion_candidates)))
+                else:
+                    logging.error('Failed to extract the symbol!') # Can't do nothing about it ...
+
+        return True, self.completion_candidates
+
+    def __code_complete_old(self, id, args):
+        original_filename = str(args[0])
+        contents_filename = str(args[1])
+        line              = int(args[2])
         column            = int(args[3])
         offset            = int(args[4])
 
         line_string = read_line(contents_filename, offset)
-        #logging.info("Filtering from {0}, {1} with offset = {2}: line = '{3}'".format(line, column, offset, line_string))
+        logging.info("Filtering from [{0}, {1}] with offset = {2}: line = '{3}'".format(line, column, offset, line_string))
 
-        tunit = self.parser.parse(contents_filename, original_filename)
-        self.auto_complete = self.parser.auto_complete(
-                    tunit, line, column + 1
-                )
-
-        del self.completion_candidates[:]
         column = column - 1 # We need 0-based index column to work with strings ...
-        #logging.info("Will extract symbol from '{0}'".format(line_string[0:column]))
-        idx = extract_symbol(line_string[0:column])
-        if idx != -1:
-            expression = line_string[(column-idx):column].rstrip()
-            #logging.info("Filtering by expression = '{0}' from '{1}' starting from {2}".format(expression, line_string[0:column], idx))
-            self.completion_candidates = self.__filter_completion_candidates(self.auto_complete.results, expression) # At this point we might already have something to work with (e.g. part of the string we may trigger filtering with)   
-            #logging.info('Found {0} candidates.'.format(len(self.completion_candidates)))
-        else:
-            logging.error('Failed to extract the symbol!') # Can't do nothing about it ...
+        logging.info("Will extract symbol from '{0}'".format(line_string[0:column]))
+        last_char_input = line_string[column-1] if column > 0 else line_string[0]
+        logging.info("last_char_input = '{0}'".format(last_char_input))
 
-        return self.auto_complete is not None, self.completion_candidates
+        if last_char_input == '\n' or last_char_input == '\r':
+            logging.info('Carriage return detected ...')
+            pass
+        elif last_char_input.isspace() or last_char_input == ',' or last_char_input == ';' or last_char_input == '=':
+            logging.info('Delimiting character detected ... Clearing up the completion candidates.')
+            del self.completion_candidates[:]
+        else:
+            logging.info('Trying harder ...')
+            idx = extract_symbol(line_string[0:column])
+            if idx != -1:
+                expression = line_string[(column-idx):column] #.rstrip()
+                logging.info("expression found = '{0}'".format(expression))
+                last_char_input = expression[len(expression)-1] if len(expression) else ''
+                logging.info("last_char_input = '{0}'".format(last_char_input))
+                if last_char_input == ':' or last_char_input == '>' or last_char_input == '.' or last_char_input == '' or len(self.completion_candidates) == 0:
+                    logging.info('Getting new list of candidates ...')
+                    del self.completion_candidates[:]
+                    tunit = self.parser.parse(contents_filename, original_filename)
+                    self.auto_complete = self.parser.auto_complete(
+                            tunit, line, column + 2
+                    )
+                    self.completion_candidates = self.__filter_completion_candidates(self.auto_complete.results, expression) # At this point we might already have something to work with (e.g. part of the string we may trigger filtering with)
+                else:
+                    logging.info("Filtering by expression = '{0}' from '{1}' starting from {2}".format(expression, line_string[0:column], idx))
+                    self.completion_candidates = self.__filter_completion_candidates(self.completion_candidates, expression) # At this point we might already have something to work with (e.g. part of the string we may trigger filtering with)   
+                logging.info('Found {0} candidates.'.format(len(self.completion_candidates)))
+            else:
+                logging.error('Failed to extract the symbol!') # Can't do nothing about it ...
+
+        return True, self.completion_candidates
 
     def __refine_candidates(self, id, args):
         original_filename = str(args[0])
