e43d83d4dee29a22fa815c4b9fcf7b3f52cd08ac
Jusufadis Bakamovic <jbakamovic@localhost.localdomain>
2018-05-11 15:59:26 +0200

    Improve input pattern matching.

--- services/source_code_model/auto_completion/auto_completion.py
+++ services/source_code_model/auto_completion/auto_completion.py
@@ -29,6 +29,7 @@ class AutoCompletion():
             SourceCodeModelAutoCompletionRequestId.REFINE_CANDIDATES : self.__refine_candidates,
         }
         self.completion_candidates = []
+        self.previous_char = ''
 
     def __call__(self, args):
         return self.op.get(int(args[0]), self.__unknown_op)(int(args[0]), args[1:len(args)])
@@ -41,43 +42,54 @@ class AutoCompletion():
         original_filename = str(args[0])
         contents_filename = str(args[1])
         line              = int(args[2])
-        column            = int(args[3]) - 1
+        column            = int(args[3])
         offset            = int(args[4]) - 1
 
         line_string = read_line(contents_filename, offset)
         logging.info("Filtering from [{0}, {1}] with offset = {2}: line = '{3}', len = {4}".format(line, column, offset, line_string, len(line_string)))
 
-        last_char_idx = column - 1
-        if last_char_idx < len(line_string):
-            last_char_input = line_string[last_char_idx-1]
-            logging.info("last_char_input = '{0}'".format(last_char_input))
-            if last_char_input == '\n' or last_char_input == '\r':
+        current_char_idx = column - 1
+        if current_char_idx < len(line_string):
+            current_char = line_string[current_char_idx]
+            logging.info("current_char = '{0}'".format(current_char))
+            if current_char == '\n' or current_char == '\r':
                 logging.info('Carriage return detected ...')
-                pass
-            elif last_char_input.isspace() or last_char_input == ',' or last_char_input == ';' or last_char_input == '=':
+                del self.completion_candidates[:]
+            elif current_char.isspace() or current_char == ',' or current_char == ';' or current_char == '=':
                 logging.info('Delimiting character detected ... Clearing up the completion candidates.')
                 del self.completion_candidates[:]
             else:
-                logging.info("Will extract symbol from '{0}'".format(line_string[0:last_char_idx]))
+                current_char_idx += 1
+                logging.info("Will extract symbol from '{0}'".format(line_string[0:current_char_idx]))
                 logging.info('Trying harder ...')
-                idx = extract_symbol(line_string[0:last_char_idx])
+                idx = extract_symbol(line_string[0:current_char_idx])
                 if idx != -1:
-                    expression = line_string[(last_char_idx-idx):last_char_idx] #.rstrip()
+                    expression = line_string[(current_char_idx-idx-1):current_char_idx]
                     logging.info("expression found = '{0}'".format(expression))
-                    last_char_input = expression[len(expression)-1] if len(expression) else ''
-                    logging.info("last_char_input = '{0}'".format(last_char_input))
-                    if last_char_input == ':' or last_char_input == '>' or last_char_input == '.' or last_char_input == '' or len(self.completion_candidates) == 0 and last_char_input != '-':
+                    current_char = expression[len(expression)-1] if len(expression) else ''
+                    logging.info("current_char = '{0}'".format(current_char))
+                    delimiter = (self.previous_char == ':' and current_char == ':') or (self.previous_char == '-' and current_char == '>') or current_char == '.'
+                    if len(self.completion_candidates) == 0 or delimiter:
                         logging.info('Getting new list of candidates ...')
                         del self.completion_candidates[:]
                         tunit = self.parser.parse(contents_filename, original_filename)
                         self.auto_complete = self.parser.auto_complete(
                                 tunit, line, column + 1
                         )
-                        self.completion_candidates = self.__filter_completion_candidates(self.auto_complete.results, expression) # At this point we might already have something to work with (e.g. part of the string we may trigger filtering with)
+                        logging.info('Clang found {0} candidates.'.format(len(self.auto_complete.results)))
+                        self.completion_candidates = self.__filter_completion_candidates(
+                            self.auto_complete.results,
+                            expression.strip() if not delimiter else ''
+                        ) # At this point we might already have something to work with (e.g. part of the string we may trigger filtering with)
                     else:
-                        logging.info("Filtering by expression = '{0}' from '{1}' starting from {2}".format(expression,
-                            line_string[0:last_char_idx], idx))
-                        self.completion_candidates = self.__filter_completion_candidates(self.completion_candidates, expression) # At this point we might already have something to work with (e.g. part of the string we may trigger filtering with)   
+                        logging.info(
+                            "Filtering by expression = '{0}' from '{1}' starting from {2}".format(expression, line_string[0:current_char_idx], idx)
+                        )
+                        self.completion_candidates = self.__filter_completion_candidates(
+                            self.completion_candidates,
+                            expression.strip() if not delimiter else ''
+                        ) # At this point we might already have something to work with (e.g. part of the string we may trigger filtering with)   
+                    self.previous_char = current_char
                     logging.info('Found {0} candidates.'.format(len(self.completion_candidates)))
                 else:
                     logging.error('Failed to extract the symbol!') # Can't do nothing about it ...
