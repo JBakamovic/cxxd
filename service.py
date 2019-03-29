af7e909f6ba85d9c11a52081fdf2e60309888f1c
Jusufadis Bakamovic <jbakam@gmail.com>
2019-03-29 09:56:35 +0100

    Remove log clutter from Services. We already log this on Server level.

--- service.py
+++ service.py
@@ -14,13 +14,13 @@ class Service():
             0x2 : self.__request
         }
         self.started_up = False
-        logging.info("Actions: {0}".format(self.action))
+        logging.debug("Actions: {0}".format(self.action))
 
     def __startup_request(self, payload):
         if self.started_up:
             logging.warning('Service is already started!')
         else:
-            logging.info("Service startup ... Payload = {0}".format(payload))
+            logging.debug("Service startup ... Payload = {0}".format(payload))
             status, startup_payload = self.startup_callback(payload)
             self.service_plugin.startup_callback(status, payload, startup_payload)
             self.started_up = status
@@ -28,7 +28,7 @@ class Service():
 
     def __shutdown_request(self, payload):
         if self.started_up:
-            logging.info("Service shutdown ... Payload = {0}".format(payload))
+            logging.debug("Service shutdown ... Payload = {0}".format(payload))
             status, shutdown_payload = self.shutdown_callback(payload)
             self.service_plugin.shutdown_callback(status, payload, shutdown_payload)
             self.started_up = not status
@@ -38,7 +38,7 @@ class Service():
 
     def __request(self, payload):
         if self.started_up:
-            logging.info("Service request ... Payload = {0}".format(payload))
+            logging.debug("Service request ... Payload = {0}".format(payload))
             success, args = self.__call__(payload)
             self.service_plugin.__call__(success, payload, args)
         else:
